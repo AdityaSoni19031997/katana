ebbb803792e54e0a8b92f79e1286051ae5d23bf0
Aditya Soni <soniaditya922@gmail.com>
2020-07-03 15:21:22 +0530

    Updating logging.conf to account for logs from __main__

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import os
 import yaml
 import logging
-import logging.config
-
 from flask import Flask, redirect
 from apis import blueprint as api
-from configurations.config import ColorFormatter
 
 with open(r"./configurations/config.yml") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api/v1')
-logging.ColorFormatter = ColorFormatter
-logging.config.fileConfig(config["LOGGING_CONFIG"])
 DEFAULT_API_VERSION = config['DEFAULT_API_VERSION']
 
 # Redirect to default namespace version
@@ -26,3 +21,8 @@ if __name__ == "__main__":
     if not os.path.exists(config["MODEL_DIR"]):
         os.makedirs(config["MODEL_DIR"], exist_ok=True)
     app.run(debug=config["DEBUG"], host=config["HOST"], port=config["PORT"])
+else:
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+ 
